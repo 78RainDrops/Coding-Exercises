ebfcd69ca5469140ec33ee10b938cd751bcdc623
Aenze <smileyrence3@gmail.com>
2025-10-07 13:46:16 +0800

    Exercise 10 calling function with positional and keyword args

---
+++ functions_exercises/exercise_10_posArgs_keyArgs.py
@@ -0,0 +1,6 @@
+def describe_pet(animal_type, pet_name):
+    print(f"Aninal type: {animal_type}, Pet Name: {pet_name}")
+
+
+describe_pet("Dog", "Doggie")
+describe_pet(animal_type="Cat", pet_name="Catty")
