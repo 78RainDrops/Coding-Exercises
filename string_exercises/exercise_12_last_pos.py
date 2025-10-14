e4cb9a49ef7877a274c180e185bf31b179b0babe
Aenze <smileyrence3@gmail.com>
2025-10-14 14:47:06 +0800

    exercise 12 find last position

---
+++ string_exercises/exercise_12_last_pos.py
@@ -0,0 +1,7 @@
+str1 = "Emma is a data scientist who knows Python. Emma works at google."
+# str1 = str1.split(" ")
+#
+# for i in range(len(str1)):
+#     if str1[i] == "Emma":
+#         print(i)
+print(str1.rfind("Emma"))
