bade49070e7fb040b2283763c26167146785cf00
Aenze <smileyrence3@gmail.com>
2025-10-18 18:58:58 +0800

    first exercise of list exercises, basic list operations

---
+++ list_exercises/exercise_1_basic_list.py
@@ -0,0 +1,8 @@
+my_list = [10, 20, 30, 40, 50]
+print(f"Initial List: {my_list}")
+print(f"Third Item: {my_list[2]}")
+print(f"Length of the list: {len(my_list)}")
+if not my_list:
+    print("List is Empty")
+else:
+    print("List is not Empty")
