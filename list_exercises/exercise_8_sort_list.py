4a7040ae89b6c953e365b037d958ad5d4122a71f
Aenze <smileyrence3@gmail.com>
2025-10-19 19:24:03 +0800

    exercise 8 sorting a list

---
+++ list_exercises/exercise_8_sort_list.py
@@ -0,0 +1,9 @@
+numbers = [5, 2, 8, 1, 9]
+
+print(f"Original List: {numbers}")
+numbers.sort()
+print(f"Sorted list: {numbers}")
+numbers = [5, 2, 8, 1, 9]
+print(f"Original List: {numbers}")
+sorted_list = sorted(numbers)
+print(f"Sorted list: {sorted_list}")
