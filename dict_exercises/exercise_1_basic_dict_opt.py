669b0a3c14c6258007f309f730e4564070a2f62b
Aenze <smileyrence3@gmail.com>
2025-10-22 10:53:49 +0800

    initial exercise for dictionary, basic dictionary operation

---
+++ dict_exercises/exercise_1_basic_dict_opt.py
@@ -0,0 +1,11 @@
+my_dict = {"name": "Alice", "age": 35, "city": "New York"}
+
+my_dict["profession"] = "Doctor"
+
+print(my_dict)
+
+my_dict["age"] = 40
+
+print(my_dict)
+
+print(f"City: {my_dict['city']}")
