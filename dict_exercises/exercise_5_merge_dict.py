9c72c981796b5cf9f01d1cff798e703859f14676
Aenze <smileyrence3@gmail.com>
2025-10-22 11:09:24 +0800

    merge dictionaries

---
+++ dict_exercises/exercise_5_merge_dict.py
@@ -0,0 +1,11 @@
+dict1 = {"Ten": 10, "Twenty": 20, "Thirty": 30}
+dict2 = {"Thirty": 30, "Fourty": 40, "Fifty": 50}
+
+dict3 = dict1 | dict2
+print(dict3)
+
+dict4 = {**dict1, **dict2}
+print(dict4)
+
+dict1.update(dict2)
+print(dict1)
