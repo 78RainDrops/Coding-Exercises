63734b594b8b53a6fab7b69c8a1a8e6dd55de205
Aenze <smileyrence3@gmail.com>
2025-10-23 20:46:14 +0800

    modify nested dictionary

---
+++ dict_exercises/exercise_9_modify_nested_dict.py
@@ -0,0 +1,5 @@
+nested_student_dict = {
+    "class": {"student": {"name": "Mike", "marks": {"physics": 70, "history": 80}}}
+}
+nested_student_dict["class"]["student"]["name"] = "Jessa"
+print(nested_student_dict)
