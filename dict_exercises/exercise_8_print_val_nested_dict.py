a8859bf3fdecdb92daa664830639eef9a10c7a63
Aenze <smileyrence3@gmail.com>
2025-10-23 20:40:25 +0800

    print the value of key history from nested dict

---
+++ dict_exercises/exercise_8_print_val_nested_dict.py
@@ -0,0 +1,6 @@
+sampleDict = {
+    "class": {"student": {"name": "Mike", "marks": {"physics": 70, "history": 80}}}
+}
+
+val_history = sampleDict["class"]["student"]["marks"]["history"]
+print(val_history)
