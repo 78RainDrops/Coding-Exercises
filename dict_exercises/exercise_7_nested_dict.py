c56ae7c147c86e1c8b2d8abf62c52ea00611bce5
Aenze <smileyrence3@gmail.com>
2025-10-23 20:36:21 +0800

    access nested dictionary

---
+++ dict_exercises/exercise_7_nested_dict.py
@@ -0,0 +1,6 @@
+data = {"person": {"name": "Alice", "age": 30}}
+
+print(f"{data['person']['name']}'s age is: {data['person']['age']}")
+
+alice_age = data["person"]["age"]
+print(f"Alice's age is: {alice_age}")
