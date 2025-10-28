170078eedd3f2a961ee263d4154d33164c39cddf
Aenze <smileyrence3@gmail.com>
2025-10-28 12:02:40 +0800

    check subset

--- set_exercises/exercise_9_check_subset.py
+++ set_exercises/exercise_9_check_subset.py
@@ -1,9 +1,9 @@
 subset_set = {10, 20}
 main_set = {10, 20, 30, 40}
-count = 0
+is_subset = True
 # print(subset_set.issubset(main_set))
-for i in main_set:
-    if count == len(subset_set):
-        print(True)
-    if i in subset_set:
-        count += 1
+for i in subset_set:
+    if i not in main_set:
+        is_subset = False
+        break
+print(is_subset)
