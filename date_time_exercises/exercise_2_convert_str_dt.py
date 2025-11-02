99ff2eb36f325eedfcb0b8d17fd5ba4aa48289ec
Aenze <smileyrence3@gmail.com>
2025-11-02 21:53:52 +0800

    convert string to datetime

---
+++ date_time_exercises/exercise_2_convert_str_dt.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+date_string = "Feb 25 2020 4:20PM"
+"""
+%Y - Year
+%m - Month
+%d - dady
+%H - hour
+%M - minutes
+%S - seconds
+"""
+date_time = datetime.strptime(date_string, "%b %d %Y %I:%M%p")
+print(date_time)
