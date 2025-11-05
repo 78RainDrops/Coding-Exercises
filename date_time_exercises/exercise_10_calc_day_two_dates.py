8aaa6085ce1367a809704ce721dfbd4439f6e7d3
Aenze <smileyrence3@gmail.com>
2025-11-05 15:45:01 +0800

    calculate dates

---
+++ date_time_exercises/exercise_10_calc_day_two_dates.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+# 2020-02-25
+date_1 = datetime(2020, 2, 25)
+
+# 2020-09-17
+date_2 = datetime(2020, 9, 17)
+
+delta = None
+if date_1 > date_2:
+    delta = date_1 - date_2
+else:
+    delta = date_2 - date_1
+
+print(delta.days)
