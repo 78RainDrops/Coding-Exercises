49fef543756f0a0c2de285fd24a98ad4baafbd4a
Aenze <smileyrence3@gmail.com>
2025-11-07 12:03:51 +0800

    class inheritance

---
+++ oop_exercises/exercise_4_inheritance.py
@@ -0,0 +1,17 @@
+class Vehicle:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+    def seating_capacity(self, capacity):
+        return f"The seating capacity of a {self.name} is {capacity} passengers"
+
+
+class Bus(Vehicle):
+    def seating_capacity(self, capacity=40):
+        return super().seating_capacity(capacity)
+
+
+bus1 = Bus("Volvo", 300, 19)
+print(bus1.seating_capacity())
