6f401cc651c3153299d005c65f1d4940f823a9ef
Aenze <smileyrence3@gmail.com>
2026-03-15 00:34:46 +0800

    valid parentheses

--- leetcode/valid_parentheses_20.py
+++ leetcode/valid_parentheses_20.py
@@ -1,20 +1,35 @@
-s = ")"
+# s = ")"
+s = "()[]{}"
 
 
 def valid_parentheses(s: str) -> bool:
-    s_stk = []
+    stk = []
     pair = {")": "(", "]": "[", "}": "{"}
-
+    print(s)
+    print("Keys: ", pair.keys())
+    print("values: ", pair.values())
     for i in s:
-        if i in pair:
-            if s_stk or s_stk[-1] == pair[i]:
-                s_stk.pop()
-            else:
+        print(i)
+        if i in pair.keys():
+            print("True for Key", i)
+            if len(stk) == 0 or stk.pop() != pair[i]:
                 return False
-        else:
-            s_stk.append(i)
+        elif i in pair.values():
+            print("true for value", i)
+            stk.append(i)
 
-    return not s_stk
+    return True if len(stk) == 0 else False
+
+    # for i in s:
+    #     if i in pair:
+    #         if s_stk or s_stk[-1] == pair[i]:
+    #             s_stk.pop()
+    #         else:
+    #             return False
+    #     else:
+    #         s_stk.append(i)
+    #
+    # return not s_stk
 
     # for i in s:
     #     if i in ("{", "[", "("):
